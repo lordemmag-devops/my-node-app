14080680bd6749e973689746481c2c83130de1ee
lordemmag-devops <lordemmag@yahoo.com>
2025-09-25 17:41:50 +0100

    complete project folder

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_artifact_registry_repository" "app_repo" {
+  location      = var.region
+  repository_id = "my-node-app-repo"
+  format        = "DOCKER"
+}
+
+resource "google_cloud_run_service" "app_service" {
+  name     = "my-node-app"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/my-node-app-repo/my-node-app:latest"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "public_access" {
+  location    = google_cloud_run_service.app_service.location
+  project     = google_cloud_run_service.app_service.project
+  service     = google_cloud_run_service.app_service.name
+
+  policy_data = jsonencode({
+    bindings = [
+      {
+        role    = "roles/run.invoker"
+        members = ["allUsers"]
+      }
+    ]
+  })
+}
+
+output "cloud_run_url" {
+  value = google_cloud_run_service.app_service.status[0].url
+}
